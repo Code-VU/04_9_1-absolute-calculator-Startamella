be735f69bee79cb432092ee375aac0aa0c969161
Samantha Tartamella <Samantha.Tartamella@vu.com>
2023-02-14 13:46:12 -0600

    oddAbsolute

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,6 +2,15 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+    n = float(in_num)
+    if n > 21:
+        result = ((n-21)*2)
+        print("Result:", int(result))
+        return(int(result))
+    elif n <= 21:
+        result = (21-n)
+        print("Result:", int(result))
+        return(int(result))
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
